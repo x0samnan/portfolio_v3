91027186a41bcd705d46dbe01b6c7318b5863bd5
Samnan <samnansiddique@yandex.com>
2023-05-07 00:47:30 +0530

    Color Scheme changed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,12 +10,12 @@ const Hero = () => {
       <div className={`${styles.paddingX} absolute inset-0 top-[120px]
       max-w-7xl mx-auto flex flex-row items-start gap-5`}>
         <div className='flex flex-col justify-center items-center mt-5'>
-          <div className='w-5 h-5 rounded-full bg-[#602796]' />
+          <div className='w-5 h-5 rounded-full bg-[#a436da]' />
           <div className='w-1 sm:h-80 h-40 violet-gradient' />
         </div>
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I am <span className='text-[#602796]'>Samnan</span>
+            Hi, I am <span className='text-[#a436da]'>Samnan</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I am a full-stack web developer
